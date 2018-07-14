aadfb48961a6acecdea25c34ce71e42ca73e1788
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-14 09:52:18 -1000

    Solution to largest palindrom product challenge.

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -5,18 +5,53 @@
  *  @return {Object} an object containing the two factors used to produce
  *                   the palindromeNumber and the palindromeNumber itself.
  */
-module.exports = function(digits){
+module.exports = function (digits) {
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
 
-  // do your work here
+  let factorCreator = function (digits) {
+    let digitScaffold = '';
+    for (let i = 0; i < digits; i++) {
+      digitScaffold += 9;
+    }
+    return Number(digitScaffold);
+  }
 
+  let factorReset = factorCreator(digits);
+  let tempFactor_0 = factorReset
+  let tempFactor_1 = factorReset;
+  let currentProduct = 0;
+  let currentString = '';
 
+  while (tempFactor_1 > 0) {
+
+    while (tempFactor_0 > 0) {
+      currentProduct = tempFactor_0 * tempFactor_1;
+      currentString = currentProduct.toString();
+      currentStringReverse = currentString.split('').reverse().join('');
+
+      if ((currentString === currentStringReverse) && (currentProduct > palindromeNumber)) {
+        palindromeNumber = currentProduct;
+        factor_0 = tempFactor_0;
+        factor_1 = tempFactor_1;
+        break;
+      }
+
+      if (currentProduct < palindromeNumber){
+        break;
+      }
+
+      tempFactor_0--;
+    }
+    
+    tempFactor_0 = factorReset;
+    tempFactor_1--;
+  }
 
   return {
-    factor_0 : factor_0,
-    factor_1 : factor_1,
-    palindromeNumber : palindromeNumber
+    factor_0: factor_0,
+    factor_1: factor_1,
+    palindromeNumber: palindromeNumber
   };
 };
